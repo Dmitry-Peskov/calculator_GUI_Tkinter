d241c527e4f663f8efcdab32d088c4a636922933
Dmitry-Peskov <dv.peskov@yandex.ru>
2022-12-11 17:15:34 +0300

    Поправил ряд ошибок

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import tkinter as tk
 
-def add_dijit(num: int):
+def add_dijit(num: str):
     '''Добавляет в поле ввода выбранную цифру'''
-    value = calc.get() + str(num)
+    value = calc.get()
+    if value[0] == '0' and len(value) == 1:
+        value = value[1:]
     calc.delete(0, tk.END)
-    calc.insert(0, value)
+    calc.insert(0, value + num)
     
 def add_operation(operation: str):
     '''Добавляет операцию в поле ввода'''
@@ -57,6 +59,7 @@ win.resizable(False, False)
 #создаём поле ввода
 calc = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 20))
 calc.grid(row=0, column=0, columnspan=4, sticky='we', padx= 10)
+calc.insert(0,'0')
 
 #настраиваем сетку
 win.grid_columnconfigure(0,minsize=90)
@@ -69,24 +72,25 @@ win.grid_rowconfigure(3,minsize=90)
 win.grid_rowconfigure(4,minsize=90)
 
 #создаём кнопки с цифрами
-create_button_dijit(7).grid(row=1,column=0, sticky='wens', padx= 1, pady=1)
-create_button_dijit(8).grid(row=1,column=1, sticky='wens', padx= 1, pady=1)
-create_button_dijit(9).grid(row=1,column=2, sticky='wens', padx= 1, pady=1)
-create_button_dijit(4).grid(row=2,column=0, sticky='wens', padx= 1, pady=1)
-create_button_dijit(5).grid(row=2,column=1, sticky='wens', padx= 1, pady=1)
-create_button_dijit(6).grid(row=2,column=2, sticky='wens', padx= 1, pady=1)
-create_button_dijit(1).grid(row=3,column=0, sticky='wens', padx= 1, pady=1)
-create_button_dijit(2).grid(row=3,column=1, sticky='wens', padx= 1, pady=1)
-create_button_dijit(3).grid(row=3,column=2, sticky='wens', padx= 1, pady=1)
-create_button_dijit(0).grid(row=4,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit('7').grid(row=1,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit('8').grid(row=1,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit('9').grid(row=1,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit('4').grid(row=2,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit('5').grid(row=2,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit('6').grid(row=2,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit('1').grid(row=3,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit('2').grid(row=3,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit('3').grid(row=3,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit('0').grid(row=4,column=1, sticky='wens', padx= 1, pady=1)
 
+#создаём кнопки с математическими операциями
 create_button_operation('/').grid(row=1,column=3, sticky='wens', padx= 1, pady=1)
 create_button_operation('*').grid(row=2,column=3, sticky='wens', padx= 1, pady=1)
 create_button_operation('-').grid(row=3,column=3, sticky='wens', padx= 1, pady=1)
 create_button_operation('+').grid(row=4,column=3, sticky='wens', padx= 1, pady=1)
 
+#создаем кнопки вычисления и очистки
 create_button_calc('=').grid(row=4,column=2, sticky='wens', padx= 1, pady=1)
-
 create_button_clear('C').grid(row=4,column=0, sticky='wens', padx= 1, pady=1)
 
 if __name__ == "__main__":
