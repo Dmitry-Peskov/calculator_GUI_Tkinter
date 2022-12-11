2b680a0ddae38e4e080df3b6baf640064205e226
Dmitry-Peskov <dv.peskov@yandex.ru>
2022-12-11 17:07:49 +0300

    Реализованы математический операции

--- main.py
+++ main.py
@@ -1,10 +1,50 @@
 import tkinter as tk
 
 def add_dijit(num: int):
-    '''Добавляеn в поле ввода выбранную цифру'''
+    '''Добавляет в поле ввода выбранную цифру'''
     value = calc.get() + str(num)
     calc.delete(0, tk.END)
     calc.insert(0, value)
+    
+def add_operation(operation: str):
+    '''Добавляет операцию в поле ввода'''
+    value = calc.get() 
+    if value[-1] in '-+*/':
+        value = value[:-1] 
+    elif '+' in value or '-' in value or '*' in value or '/' in value:
+        calculate()
+        value = calc.get()
+    calc.delete(0, tk.END)
+    calc.insert(0, value + operation)
+
+def calculate():
+    '''Производит вычеснение на основе выражения записанного в строке ввода'''
+    value = calc.get()
+    if value[-1] in '+-*/':
+        value = value + value[:-1]
+    calc.delete(0, tk.END)
+    calc.insert(0, eval(value))
+
+def clear():
+    '''Очищает поле ввода'''
+    calc.delete(0, tk.END)
+    calc.insert(0,0)
+
+def create_button_dijit(num: int):
+    '''Создаёт кнопку'''
+    return tk.Button(text= str(num), font=('Arial',20), bd=2,command= lambda : add_dijit(num))
+
+def create_button_operation(operation: str):
+    '''Создаёт кнопки математических операций'''
+    return tk.Button(text= str(operation), font=('Impact',20), bd=2,command= lambda : add_operation(operation))
+
+def create_button_calc(operation: str):
+    '''Создаёт кнопку равенства'''
+    return tk.Button(text= str(operation), font=('Impact',20), bd=2,command= calculate)
+
+def create_button_clear(operation: str):
+    '''Создаёт кнопку равенства'''
+    return tk.Button(text= str(operation), font=('Impact',20), bd=2,command= clear)
 
 #Настройки галвного окна
 win = tk.Tk()
@@ -16,29 +56,38 @@ win.resizable(False, False)
 
 #создаём поле ввода
 calc = tk.Entry(win, justify=tk.RIGHT, font=('Arial', 20))
-calc.grid(row=0, column=0, columnspan=3, sticky='we')
+calc.grid(row=0, column=0, columnspan=4, sticky='we', padx= 10)
 
 #настраиваем сетку
-win.grid_columnconfigure(0,minsize=100)
-win.grid_columnconfigure(1,minsize=100)
-win.grid_columnconfigure(2,minsize=100)
-win.grid_rowconfigure(1,minsize=100)
-win.grid_rowconfigure(2,minsize=100)
-win.grid_rowconfigure(3,minsize=100)
-win.grid_rowconfigure(4,minsize=100)
+win.grid_columnconfigure(0,minsize=90)
+win.grid_columnconfigure(1,minsize=90)
+win.grid_columnconfigure(2,minsize=90)
+win.grid_columnconfigure(3,minsize=90)
+win.grid_rowconfigure(1,minsize=90)
+win.grid_rowconfigure(2,minsize=90)
+win.grid_rowconfigure(3,minsize=90)
+win.grid_rowconfigure(4,minsize=90)
 
+#создаём кнопки с цифрами
+create_button_dijit(7).grid(row=1,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit(8).grid(row=1,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit(9).grid(row=1,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit(4).grid(row=2,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit(5).grid(row=2,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit(6).grid(row=2,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit(1).grid(row=3,column=0, sticky='wens', padx= 1, pady=1)
+create_button_dijit(2).grid(row=3,column=1, sticky='wens', padx= 1, pady=1)
+create_button_dijit(3).grid(row=3,column=2, sticky='wens', padx= 1, pady=1)
+create_button_dijit(0).grid(row=4,column=1, sticky='wens', padx= 1, pady=1)
 
-tk.Button(text='7', font=('Arial',20), bd=2,command= lambda : add_dijit(7)).grid(row=1,column=0, sticky='wens', padx= 1, pady=1)
-tk.Button(text='8', font=('Arial',20), bd=2,command= lambda : add_dijit(8)).grid(row=1,column=1, sticky='wens', padx= 1, pady=1)
-tk.Button(text='9', font=('Arial',20), bd=2,command= lambda : add_dijit(9)).grid(row=1,column=2, sticky='wens', padx= 1, pady=1)
-tk.Button(text='4', font=('Arial',20), bd=2,command= lambda : add_dijit(4)).grid(row=2,column=0, sticky='wens', padx= 1, pady=1)
-tk.Button(text='5', font=('Arial',20), bd=2,command= lambda : add_dijit(5)).grid(row=2,column=1, sticky='wens', padx= 1, pady=1)
-tk.Button(text='6', font=('Arial',20), bd=2,command= lambda : add_dijit(6)).grid(row=2,column=2, sticky='wens', padx= 1, pady=1)
-tk.Button(text='1', font=('Arial',20), bd=2,command= lambda : add_dijit(1)).grid(row=3,column=0, sticky='wens', padx= 1, pady=1)
-tk.Button(text='2', font=('Arial',20), bd=2,command= lambda : add_dijit(2)).grid(row=3,column=1, sticky='wens', padx= 1, pady=1)
-tk.Button(text='3', font=('Arial',20), bd=2,command= lambda : add_dijit(3)).grid(row=3,column=2, sticky='wens', padx= 1, pady=1)
-tk.Button(text='0', font=('Arial',20), bd=2,command= lambda : add_dijit(0)).grid(row=4,column=1, sticky='wens', padx= 1, pady=1)
+create_button_operation('/').grid(row=1,column=3, sticky='wens', padx= 1, pady=1)
+create_button_operation('*').grid(row=2,column=3, sticky='wens', padx= 1, pady=1)
+create_button_operation('-').grid(row=3,column=3, sticky='wens', padx= 1, pady=1)
+create_button_operation('+').grid(row=4,column=3, sticky='wens', padx= 1, pady=1)
 
+create_button_calc('=').grid(row=4,column=2, sticky='wens', padx= 1, pady=1)
+
+create_button_clear('C').grid(row=4,column=0, sticky='wens', padx= 1, pady=1)
 
 if __name__ == "__main__":
     win.mainloop()
